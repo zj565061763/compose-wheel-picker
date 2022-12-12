677d81bfec29c527ef225c4cf8e226579c732744
zj565061763 <565061763@qq.com>
2022-12-12 16:46:19 +0800

    simplify

--- lib/src/main/java/com/sd/lib/compose/wheel_picker/FWheelPicker.kt
+++ lib/src/main/java/com/sd/lib/compose/wheel_picker/FWheelPicker.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -132,17 +134,11 @@ private fun WheelPicker(
         itemSize * (unfocusedCount * 2 + 1)
     }
 
-    val contentUpdated by rememberUpdatedState(content)
-    val contentScope = remember(state) { WheelPickerContentScopeImpl(state) }
-    val contentWrapperScope = remember(contentScope) {
-        object : FWheelPickerContentWrapperScope {
-            override val state: FWheelPickerState get() = contentScope.state
-
-            @Composable
-            override fun content(index: Int) {
-                contentUpdated.invoke(contentScope, index)
-            }
-        }
+    val contentWrapperScope = remember(state) {
+        val contentScope = WheelPickerContentScopeImpl(state)
+        FWheelPickerContentWrapperScopeImpl(contentScope)
+    }.apply {
+        this.content = content
     }
 
     Box(
@@ -180,7 +176,7 @@ private fun WheelPicker(
                     isVertical = isVertical,
                     itemSize = itemSize,
                 ) {
-                    contentWrapper.invoke(contentWrapperScope, index)
+                    contentWrapperScope.contentWrapper(index)
                 }
             }
 
@@ -297,6 +293,18 @@ private class WheelPickerContentScopeImpl(
     override val state: FWheelPickerState,
 ) : FWheelPickerContentScope
 
+private class FWheelPickerContentWrapperScopeImpl(
+    private val contentScope: FWheelPickerContentScope
+) : FWheelPickerContentWrapperScope {
+    lateinit var content: @Composable FWheelPickerContentScope.(index: Int) -> Unit
+
+    override val state: FWheelPickerState get() = contentScope.state
+
+    @Composable
+    override fun content(index: Int) {
+        contentScope.content(index)
+    }
+}
 
 internal inline fun logMsg(debug: Boolean, block: () -> String) {
     if (debug) {
