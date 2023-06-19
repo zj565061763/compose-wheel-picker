65b8f16960707eb0f8ac508a1ccd63f1d3d97cea
zj565061763 <565061763@qq.com>
2023-06-19 13:43:36 +0800

    1.0.0-alpha17

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-wheel-picker"
-val libVersionName = "1.0.0-alpha16"
+val libVersionName = "1.0.0-alpha17"
 
 android {
     namespace = "com.sd.lib.compose.wheel_picker"
