bb5ed2a3c50cd5cb2a474134b3c58661c5711641
zjun <565061763@qq.com>
2024-02-12 14:30:58 +0800

    1.0.0-beta04

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-wheel-picker"
-val libVersionName = "1.0.0-beta03"
+val libVersionName = "1.0.0-beta04"
 
 android {
     namespace = "com.sd.lib.compose.wheel_picker"
